d4ea6198732b4ba636355e5b3e3f43d36a5c5e2c
Saif-Mahmud-Khan-Dourjoy <saifmahmudkhandourjoy@gmail.com>
2021-12-19 02:12:55 +0600

    tags

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -11,9 +11,27 @@ class IndexController extends Controller
     public function index(){
         $featured=Product::where('featured',1)->where('status',1)->get();
         $hot_deals=Product::where('hot_deals',1)->where('status',1)->get();
-        $special_offer=Product::where('special_offer',1)->where('status',1)->get();
-        $special_deals=Product::where('special_deals',1)->where('status',1)->get();
-        return view('frontend.index',compact('featured','hot_deals','special_offer','special_deals'));
+        $special_offer=Product::where('special_offer',1)->where('status',1)->where('discount_price','!=',NULL)->get();
+        $special_deals=Product::where('special_deals',1)->where('status',1)->where('discount_price','!=',NULL)->get();
+        $product=Product::where('status',1)->get();
+        $final_tag_en=[];
+        $final_tag_bn=[];
+        foreach($product as $pro){
+            $tags_en=$pro->product_tags_en;
+            $tags_bn=$pro->product_tags_bn;
+            $tags_en_arr=explode(",",$tags_en);
+            $tags_bn_arr=explode(",",$tags_bn);
+            array_push($final_tag_en,...$tags_en_arr);
+            array_push($final_tag_bn,...$tags_bn_arr);
+            
+            
+        }
+      
+        $final_tag_en_arr=array_unique($final_tag_en);
+      
+        $final_tag_bn_arr=array_unique($final_tag_bn);    
+        
+        return view('frontend.index',compact('featured','hot_deals','special_offer','special_deals','final_tag_en_arr','final_tag_bn_arr'));
     }
     public function single_product($id,$slug){
         $product=Product::findOrFail($id);
---
+++ resources/views/frontend/inc/product_tag.blade.php
@@ -0,0 +1,18 @@
+<div class="sidebar-widget product-tag wow fadeInUp">
+	<h3 class="section-title">Product tags</h3>
+	<div class="sidebar-widget-body outer-top-xs">
+		<div class="tag-list">	
+		@if(session()->get('language')=='english')
+		  @foreach($final_tag_en_arr as $item)
+          <a class="item active" title="Phone" href="category.html">{{$item}}</a>
+          @endforeach
+		@else
+		 @foreach($final_tag_bn_arr as $item)
+          <a class="item active" title="Phone" href="category.html">{{$item}}</a>
+          @endforeach
+		@endif				
+			
+
+		</div><!-- /.tag-list -->
+	</div><!-- /.sidebar-widget-body -->
+</div>
